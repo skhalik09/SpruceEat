28c96e6d0233093775b7987c32c81a6e0f3a314e
skhalik09 <zackkhalik@gmail.com>
2023-06-09 22:55:12 -0500

    adding files2

---
+++ src/test/java/com/test/bank/test/BankLoginTest.java
@@ -0,0 +1,29 @@
+package com.test.bank.test;
+
+import com.test.bank.pages.BankLoginPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class BankLoginTest extends BankTestBase{
+    @Test
+    public void validateLoginPageComponent() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+        BankLoginPage bankLoginPage = new BankLoginPage(driver);
+        bankLoginPage.LoginPageComponentsValidation("XYZ Bank");
+        Thread.sleep(2000);
+        bankLoginPage.clickManagerButton();
+
+    }
+}
