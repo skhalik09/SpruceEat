72e8797309a3afcb72789f9554648fc75cad8de2
skhalik09 <zackkhalik@gmail.com>
2023-06-09 22:48:21 -0500

    adding files

--- src/test/java/Utils/BrowserUtils.java
+++ src/test/java/Utils/BrowserUtils.java
@@ -1,8 +1,12 @@
 package Utils;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+
+import java.util.Set;
 
 public class BrowserUtils {
     public static String getText (WebElement element){
@@ -25,8 +29,43 @@ public class BrowserUtils {
                 Assert.assertTrue(false);
         }
 
+    }
 
+    public  static  String getTitleWithJS(WebDriver driver){
 
+        JavascriptExecutor js = (JavascriptExecutor)  driver;
+        return js.executeScript("return.document.title").toString().trim();
+    }
+
+    public static void clickWithJs(WebDriver driver,WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click()",element);
+    }
+
+    public static  void scrollWithJs(WebDriver driver , WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true)",element);
+    }
+    public static void switchById(WebDriver driver){
+        String mainPageID0 = driver.getWindowHandle(); //my current driver page ID
+        Set<String> allPagesID = driver.getWindowHandles();
+        for (String id : allPagesID){
+            if (!id.equals(mainPageID0)){
+                driver.switchTo().window(id);
+
+            }
+    }
+
+    }
+
+    public static void switchByTitle(WebDriver driver,String title){
+        Set<String> allPagesID = driver.getWindowHandles();//return all pages ID(second or 3rd page)
+        for (String id: allPagesID){
+            driver.switchTo().window(id);
+            if (driver.getTitle().contains(title)){
+                break;
+            }
+        }
 
     }
 }
